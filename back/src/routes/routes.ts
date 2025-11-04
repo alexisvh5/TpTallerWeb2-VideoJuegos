ccfecf7cbdd19073a8cddf60ba40df1359ad346a
garin25 <garinchristian4@gmail.com>
2025-11-04 15:50:20 -0300

    correccion en rutas

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import empleadoRouter from "./empleado-router/empleado.routes.js";
+import usuariosRouter from "./usuario-.router/usuario.routes.js";
 
 export class AppRoutes {
 
@@ -7,7 +7,7 @@ export class AppRoutes {
 
         const  router = Router();
 
-        router.use('/api/empleado',empleadoRouter)
+        router.use('/api/usuarios',usuariosRouter)
 
         return router;
     }
--- back/src/routes/usuario-.router/usuario.routes.ts
+++ back/src/routes/usuario-.router/usuario.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { UsuarioController } from "../../controllers/usuario.controller.js";
 
-const router = Router();
+const usuariosRouter = Router();
 const usuarioController = new UsuarioController();
 
 // Rutas
-router.post("/signup", (req, res) => usuarioController.signup(req, res));
-router.post("/login", (req, res) => usuarioController.login(req, res));
-router.get("/", (req, res) => usuarioController.getAll(req, res));
+usuariosRouter.post("/signup", (req, res) => usuarioController.signup(req, res));
+usuariosRouter.post("/login", (req, res) => usuarioController.login(req, res));
+usuariosRouter.get("/", (req, res) => usuarioController.getAll(req, res));
 
-export default router;
+export default usuariosRouter;
