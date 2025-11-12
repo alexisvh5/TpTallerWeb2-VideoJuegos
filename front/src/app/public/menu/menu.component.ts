4fa473f2081dad39e65643fba5f3d04f811d803d
TomasZ-png <animacionesc4d@gmail.com>
2025-11-12 02:07:24 -0300

    mejora del menu del home

--- front/src/app/public/menu/menu.component.ts
+++ front/src/app/public/menu/menu.component.ts
@@ -1,23 +1,49 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../footer/footer.component";
 import { HomeComponent } from "../home/home.component";
 import { AutenticacionService } from '../../api/services/autenticacion/autenticacion.service';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink, FooterComponent, RouterOutlet],
+  imports: [MenubarModule, InputTextModule, AvatarModule, RouterOutlet],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
-
+export class MenuComponent implements OnInit {
+  items: MenuItem[] = [];
   auth = inject(AutenticacionService);
   router = inject(Router);
 
-  logout(){
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        command: () => this.router.navigate(['/home'])
+      },
+      {
+        label: 'Juegos',
+        icon: 'pi pi-star',
+        items: [
+          { label: 'Lista de juegos', icon: 'pi pi-list', command: () => this.router.navigate(['/home']) },
+          { label: 'Ofertas', icon: 'pi pi-percentage', command: () => alert('Ofertas próximamente') }
+        ]
+      },
+      {
+        label: 'Salir',
+        icon: 'pi pi-sign-out',
+        command: () => this.logout()
+      }
+    ];
+  }
+
+  logout() {
     this.auth.logout();
     this.router.navigate(['/login']);
   }
-
 }
